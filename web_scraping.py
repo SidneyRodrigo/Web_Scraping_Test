5a5ff7ae511d3165c888c64564d019d3c4f945e7
Sidney Rodrigo <126164198+SidneyRodrigo@users.noreply.github.com>
2023-08-31 14:33:47 -0300

    Update web_scraping.py

--- web_scraping.py
+++ web_scraping.py
@@ -2,48 +2,48 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
-# URL da página que queremos fazer scraping
+# URL of the page we want to scrape
 url = "https://www.imdb.com/chart/top/"
 
-
+# User-Agent header to mimic a browser request
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
 }
 
-# Conexão: Enviar uma solicitação GET para a URL
+# Making a GET request to the URL
 response = requests.get(url, headers=headers)
 
-# Verificar se a solicitação foi bem-sucedida (status 200)
+# Checking if the request was successful (status 200)
 if response.status_code == 200:
-    # Parse a página com o BeautifulSoup
+    # Parsing the page with BeautifulSoup
     soup = BeautifulSoup(response.text, "html.parser")
     
-    # Encontre os elementos HTML que contêm os títulos de notícias
+    # Finding HTML elements that contain movie details
     imdbs = soup.find_all("div", class_="sc-b85248f1-0 bCmTgE cli-children")
     
-    # Crie arquivo CSV
+    # Creating a CSV file
     file = open('export_data.csv', 'w', newline='', encoding='utf-8')
     writer = csv.writer(file)
-    headers = ['Nome do Filme', 'Ano de Produção', 'Nota do Filme']
+    headers = ['Movie Title', 'Production Year', 'Movie Rating']
     writer.writerow(headers)
     
-    # Loop pelos elementos e imprimir os títulos
+    # Looping through the elements and extracting information
     for imdb in imdbs:
-        # Extrair o título do filme
+        # Extracting the movie title
         title = imdb.find("h3", class_="ipc-title__text").text.strip()
         
-        # Extrair o ano de produção do filme
+        # Extracting the production year of the movie
         year = imdb.find("span", class_="sc-b85248f1-6 bnDqKN cli-title-metadata-item").text.strip()
                 
-        # Extrair a nota do filme
+        # Extracting the movie rating
         stars = imdb.find("span", class_="ipc-rating-star ipc-rating-star--base ipc-rating-star--imdb ratingGroup--imdb-rating").text.strip()
         
-        # Escrever as informações no arquivo CSV
+        # Writing the information to the CSV file
         row = [title, year, stars]
         writer.writerow(row)
         
     file.close()
-    print("Dados exportados para export_data.csv.")
+    print("Data exported to export_data.csv.")
 
 else:
-    print("Falha ao acessar a página:", response.status_code)
+    print("Failed to access the page:", response.status_code)
